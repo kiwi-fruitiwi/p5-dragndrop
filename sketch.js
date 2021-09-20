5cfda11e83bbfc1642ba282f87002f59cb175e73
kiwi-fruitiwi <zane.tian@gmail.com>
2021-09-20 01:15:08 -0700

    Add empty template project

---
+++ sketch.js
@@ -0,0 +1,22 @@
+/*
+@author Kiwi
+@date 2021-09-20
+
+Exploring drag and drop with code from
+	https://editor.p5js.org/enickles/sketches/H1n19TObz
+ */
+let font
+
+function preload() {
+    font = loadFont('fonts/Meiryo-01.ttf')
+}
+
+function setup() {
+    createCanvas(640, 360)
+    colorMode(HSB, 360, 100, 100, 100)
+    background(0, 0, 30)
+}
+
+function draw() {
+
+}
